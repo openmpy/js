f8a5290f7b73dcd6ac84550385bb3fa7712db70d
openmpy <suhwan@kakao.com>
2024-03-22 21:59:59 +0900

    테스트

--- test.js
+++ test.js
@@ -9,9 +9,9 @@ async function signIn() {
         const response = await fetch(url, {
             method: 'POST', // 요청 방식 설정
             headers: {
-                'Accept': '*/*', // 서버가 응답할 수 있는 컨텐츠 유형
                 'Content-Type': 'application/json' // 보내는 데이터의 컨텐츠 유형
             },
+            credentials: 'include',
             body: JSON.stringify(data) // JavaScript 객체를 JSON 문자열로 변환
         });
 
@@ -28,54 +28,54 @@ async function signIn() {
 
 signIn(); // 함수 호출
 
-async function logout() {
-    const url = 'http://hanghae-4.ap-northeast-2.elasticbeanstalk.com/api/v1/members/logout';
+// async function logout() {
+//     const url = 'http://hanghae-4.ap-northeast-2.elasticbeanstalk.com/api/v1/members/logout';
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST', // 요청 방식 설정
-            headers: {
-                'Accept': '*/*', // 서버가 응답할 수 있는 컨텐츠 유형
-                'Content-Type': 'application/json' // 보내는 데이터의 컨텐츠 유형
-            }
-        });
+//     try {
+//         const response = await fetch(url, {
+//             method: 'POST', // 요청 방식 설정
+//             headers: {
+//                 'Accept': '*/*', // 서버가 응답할 수 있는 컨텐츠 유형
+//                 'Content-Type': 'application/json' // 보내는 데이터의 컨텐츠 유형
+//             }
+//         });
 
-        if (!response.ok) {
-            throw new Error('Request failed with status ' + response.status);
-        }
+//         if (!response.ok) {
+//             throw new Error('Request failed with status ' + response.status);
+//         }
 
-        const result = await response.json(); // 응답 본문을 JSON으로 파싱
-        console.log(result); // 결과 출력
-    } catch (error) {
-        console.error('Error during the fetch operation: ', error);
-    }
-}
+//         const result = await response.json(); // 응답 본문을 JSON으로 파싱
+//         console.log(result); // 결과 출력
+//     } catch (error) {
+//         console.error('Error during the fetch operation: ', error);
+//     }
+// }
 
 // logout();
 
 
-async function ping() {
-    const url = 'http://localhost:8080/cookie';
+// async function ping() {
+//     const url = 'http://localhost:8080/cookie';
 
-    try {
-        const response = await fetch(url, {
-            method: 'GET', // 요청 방식 설정
-            headers: {
-                'Accept': '*/*', // 서버가 응답할 수 있는 컨텐츠 유형
-                'Content-Type': 'application/json' // 보내는 데이터의 컨텐츠 유형
-            },
-            credentials: 'include'
-        });
+//     try {
+//         const response = await fetch(url, {
+//             method: 'GET', // 요청 방식 설정
+//             headers: {
+//                 'Accept': '*/*', // 서버가 응답할 수 있는 컨텐츠 유형
+//                 'Content-Type': 'application/json' // 보내는 데이터의 컨텐츠 유형
+//             },
+//             credentials: 'include'
+//         });
 
-        if (!response.ok) {
-            throw new Error('Request failed with status ' + response.status);
-        }
+//         if (!response.ok) {
+//             throw new Error('Request failed with status ' + response.status);
+//         }
 
-        // const result = await response.json(); // 응답 본문을 JSON으로 파싱
-        // console.log(result); // 결과 출력
-    } catch (error) {
-        console.error('Error during the fetch operation: ', error);
-    }
-}
+//         // const result = await response.json(); // 응답 본문을 JSON으로 파싱
+//         // console.log(result); // 결과 출력
+//     } catch (error) {
+//         console.error('Error during the fetch operation: ', error);
+//     }
+// }
 
 // ping();
